092cf7f78b379364235e50c073cb49dae066e409
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2023-06-29 23:26:51 +0200

    we love serive workers??

--- ascella-web/astro.config.mjs
+++ ascella-web/astro.config.mjs
@@ -3,12 +3,59 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 import htmlMinify from "@frontendista/astro-html-minify";
+import AstroPWA from '@vite-pwa/astro'
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx(),htmlMinify()],
+  integrations: [
+    svelte(),
+    tailwind(),
+    mdx(),
+    htmlMinify(),
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Ascella the image uplaoder',
+        short_name: 'Ascella',
+        theme_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        prefer_related_applications: false,
+        icons: [
+          {
+            src: 'pwa-192x192.avif',
+            sizes: '192x192',
+            type: 'image/avif',
+          },
+          {
+            src: 'pwa-512x512.avif',
+            sizes: '512x512',
+            type: 'image/avif',
+          },
+          {
+            src: 'pwa-512x512.avif',
+            sizes: '512x512',
+            type: 'image/avif',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/404',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/404$/],
+      },
+    })
+  ],
   output: "server",
   adapter: cloudflare({
     mode: "directory"
--- ascella-web/src/components/UploadForm.svelte
+++ ascella-web/src/components/UploadForm.svelte
@@ -7,44 +7,65 @@
   let fileInputElement: HTMLInputElement;
 
   onMount(() => {
+
+    if ("launchQueue" in window) {
+      window.launchQueue.setConsumer((launchParams) => {
+        if (launchParams.files && launchParams.files.length) {
+          fileInputElement.files = launchParams.files;
+        }
+      });
+    }
+
     window.onbeforeunload = s => (fileInputElement.value && progress <= 99) ? "" : null;
   });
 
   async function handleFormSubmit(event: SubmitEvent) {
-    const config = getConfig();
-    //@ts-ignore
-    const formdata = new FormData(event.target);
-    const headers = getHeadersFromConfig(config);
 
     const xhr = new XMLHttpRequest();
+
     xhr.upload.addEventListener("progress", e => {
       progress = (e.loaded / e.total) * 100;
     });
+
     xhr.open("POST", emptyConfig.RequestURL, true);
+
+    const headers = getHeadersFromConfig(getConfig());
+
     for (const [key, value] of Object.entries(headers)) {
-      xhr.setRequestHeader(key, value);
+      xhr.setRequestHeader(encodeURI(key), encodeURI(value));
     }
-    xhr.send(formdata);
+
+    //@ts-ignore
+    xhr.send(new FormData(event.target));
+
     xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          const json = JSON.parse(xhr.responseText);
-          window.location.href = `/success?d=${json.delete}&c=${json.url}`;
-        } else {
-          console.error(xhr.responseText);
-        }
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const json = JSON.parse(xhr.responseText);
+        window.location.href = `/success?d=${json.delete}&c=${json.url}`;
+      } else {
+        console.error(xhr.responseText);
       }
+
     };
 
   }
+
+  let uploadDisabled = true;
+
+
+  function updateButton() {
+    uploadDisabled = !fileInputElement.files.length || progress !== -1;
+  }
+
+
 </script>
 
 <form on:submit|preventDefault={handleFormSubmit} class="mx-auto grid max-w-lg gap-2">
   <input on:dragover|preventDefault on:dragenter|preventDefault
          on:drop|preventDefault={e => {fileInputElement.files = e.dataTransfer.files}} accept="image/*"
-         bind:this={fileInputElement} disabled={progress !== -1} type="file" name="file"
+         bind:this={fileInputElement} on:change={updateButton} disabled={progress !== -1} type="file" name="file"
          class="file-input file-input-primary" />
-  <button disabled={progress !== -1} type="submit" class="btn btn-primary">Upload</button>
+  <button disabled={uploadDisabled} type="submit" class="btn btn-primary">Upload</button>
   {#if progress > -1}
     <div class=" w-full h-6 bg-base-200 b-base-100 drop-shadow-lg rounded">
       <div class="transition-all duration-400 ease-in-out h-full bg-primary rounded" style="width: {progress}%"></div>
