af9f82a491e70014f07c9eb3022c575e7359dd1e
Tricked <tricked@tricked.pro>
2023-07-04 16:41:25 +0200

    decoding

--- ascella-web/src/components/ConfigWizard.svelte
+++ ascella-web/src/components/ConfigWizard.svelte
@@ -21,13 +21,12 @@
 
   function save(): void {
     console.log("saving config to localstorage");
-    config.meta_domain = domain
-    config.meta_subdomain = subdomain
+    config.meta_domain = domain;
+    config.meta_subdomain = subdomain;
     localStorage.setItem("config", JSON.stringify(config));
   }
 
   async function submit(): Promise<void> {
-
     error = "";
 
     // check if the user has accepted the rules
@@ -52,14 +51,14 @@
     // download or upload the config
     const file = {
       ...emptyConfig,
-      Headers: getHeadersFromConfig(config)
+      Headers: getHeadersFromConfig(config),
     };
 
     if (uploadToDesktop) {
       try {
         const result = await fetch("http://localhost:3234", {
           method: "POST",
-          body: JSON.stringify(file)
+          body: JSON.stringify(file),
         });
         if (!result.ok) {
           error = `${result.status} : ${result.statusText}`;
@@ -72,7 +71,6 @@
         }
       }
       uploadToDesktop = false;
-
     } else {
       const json = JSON.stringify(file);
       const blob = new Blob([json], { type: "octet/stream" });
@@ -85,8 +83,6 @@
       window.URL.revokeObjectURL(url); // ts doesn't know about window.URL again
     }
   }
-
-
 </script>
 
 <div class=" sm:max-w-full rounded-sm">
@@ -96,7 +92,9 @@
   </marquee>
   <div class="flex mh-12 justify-center min-w-full text-red-950 bg-yellow-200 transition-all">{error}</div>
   <form
-    on:change={()=>{save()}}
+    on:change={() => {
+      save();
+    }}
     class="flex flex-wrap md:grid md:grid-cols-4 gap-4"
     on:submit|preventDefault={async () => {
       await submit();
@@ -108,39 +106,40 @@
       </marquee>
       <div class="grid grid-cols-2 gap-2 w-full">
         {#if domain === "custom"}
-
           <a target="_blank" href="/custom-domain-help" class="col-span-2">
             How to set up your domain for Ascella
             <OpenInNew class="inline-block" />
           </a>
 
-          <input class="input input-primary rounded-sm"
-                 bind:value={config.domain}
-                 placeholder="your.domain.com" />
-
+          <input class="input input-primary rounded-sm" bind:value={config.domain} placeholder="your.domain.com" />
         {:else}
           <p class="col-span-2">Optional custom subdomain</p>
 
-          <input pattern="^[a-zA-Z0-9][a-zA-Z0-9.-]*[a-zA-Z0-9]$" class="input input-primary rounded-sm"
-                 placeholder="subdomain."
-                 bind:value={subdomain}
-                 on:focusout={() => {
-                    if (!subdomain.endsWith('.') && subdomain !== '') {
-                      subdomain += '.'
-                    }
-                 }}
+          <input
+            pattern="^[a-zA-Z0-9][a-zA-Z0-9.-]*[a-zA-Z0-9]$"
+            class="input input-primary rounded-sm"
+            placeholder="subdomain."
+            bind:value={subdomain}
+            on:focusout={() => {
+              if (!subdomain.endsWith(".") && subdomain !== "") {
+                subdomain += ".";
+              }
+            }}
           />
-
         {/if}
 
-        <select bind:value={domain} on:change={()=> {
-          if (domain === "custom") {
-            config.domain = ''
-          } else {
-            config.domain = domain;
-          }
-
-        }} class="select select-primary rounded-sm" required>
+        <select
+          bind:value={domain}
+          on:change={() => {
+            if (domain === "custom") {
+              config.domain = "";
+            } else {
+              config.domain = domain;
+            }
+          }}
+          class="select select-primary rounded-sm"
+          required
+        >
           {#await domains then domains}
             {#each domains as domain}
               <option selected={domain === domain.domain}>{domain.domain}</option>
@@ -149,7 +148,6 @@
           <option value="custom" selected={domain === "custom"}>Custom</option>
         </select>
       </div>
-
     </div>
     <div class="form-control w-full my-2 col-span-2">
       <label class="animate-pulse">Ascella token</label>
@@ -159,15 +157,19 @@
           logging in here with discord
           <OpenInNew class="inline-block align-middle" />
         </a>
-
       </p>
-      <input bind:value={config.token} placeholder="ascella-token"
-             class="input input-accent rounded-sm focus:translate-x-4" />
+      <input bind:value={config.token} placeholder="ascella-token" class="input input-accent rounded-sm focus:translate-x-4" />
     </div>
     <div class="form-control w-full col-span-2">
       <p>Auto Delete Image days {config.days}</p>
-      <input aria-label="Auto delete days silder" bind:value={config.days} type="range" min="1" max="365"
-             class="range range-secondary hover:range-primary duration-500" />
+      <input
+        aria-label="Auto delete days silder"
+        bind:value={config.days}
+        type="range"
+        min="1"
+        max="365"
+        class="range range-secondary hover:range-primary duration-500"
+      />
     </div>
     <div class="form-control w-full my-2 col-span-2">
       <label class="animate-ping animate-pulse">Url Style</label>
@@ -183,33 +185,24 @@
     <details class="col-span-4">
       <summary> Advanced options</summary>
       <div class="grid grid-cols-4 gap-4">
-
         <div class="form-control w-full my-2 col-span-2">
           <label class="animate-pulse">Append</label>
-          <input bind:value={config.append}
-                 placeholder="append"
-                 class="input input-accent rounded-sm focus:translate-x-4" />
+          <input bind:value={config.append} placeholder="append" class="input input-accent rounded-sm focus:translate-x-4" />
         </div>
 
         <div class="form-control w-full my-2 col-span-1">
           <label class="animate-pulse">Custom Vanity Length</label>
-          <input bind:value={config.length} type="number"
-                 placeholder="length"
-                 class="input input-accent rounded-sm focus:translate-y-4" />
+          <input bind:value={config.length} type="number" placeholder="length" class="input input-accent rounded-sm focus:translate-y-4" />
         </div>
 
         <div class="form-control w-full my-2 col-span-1">
           <label class="animate-pulse">Custom Extension</label>
-          <input bind:value={config.ext}
-                 placeholder="ext"
-                 class="input input-accent rounded-sm focus:translate-x-8" />
+          <input bind:value={config.ext} placeholder="ext" class="input input-accent rounded-sm focus:translate-x-8" />
         </div>
 
         <div class="form-control w-full my-2">
           <label class="animate-pulse">Embed Title</label>
-          <input bind:value={config.embed.title}
-                 placeholder="title"
-                 class="input input-accent rounded-sm focus:translate-y-4" />
+          <input bind:value={config.embed.title} placeholder="title" class="input input-accent rounded-sm focus:translate-y-4" />
         </div>
 
         <div class="form-control w-full my-2 col-span-2">
@@ -217,50 +210,52 @@
           <input
             bind:value={config.embed.description}
             placeholder="description"
-            class="input input-accent rounded-sm focus:translate-x-4" />
+            class="input input-accent rounded-sm focus:translate-x-4"
+          />
         </div>
 
         <div class="form-control w-full my-2">
           <label class="animate-pulse">Embed Color</label>
-          <input bind:value={config.embed.color}
-                 placeholder="color"
-                 class="input input-accent rounded-sm focus:translate-y-10" />
+          <input bind:value={config.embed.color} placeholder="color" class="input input-accent rounded-sm focus:translate-y-10" />
         </div>
 
         <div class="form-control w-full my-2">
           <label class="animate-pulse">Embed Site Name</label>
-          <input bind:value={config.embed.sitename}
-                 placeholder="sitename"
-                 class="input input-accent rounded-sm focus:translate-y-2" />
+          <input bind:value={config.embed.sitename} placeholder="sitename" class="input input-accent rounded-sm focus:translate-y-2" />
         </div>
 
         <div class="form-control w-full my-2">
           <label class="animate-pulse">Embed Site Name URL</label>
-          <input bind:value={config.embed["sitename-url"]}
-                 placeholder="sitename-url"
-                 class="input input-accent rounded-sm focus:translate-y-2" />
+          <input
+            bind:value={config.embed["sitename-url"]}
+            placeholder="sitename-url"
+            class="input input-accent rounded-sm focus:translate-y-2"
+          />
         </div>
 
         <div class="form-control w-full my-2">
           <label class="animate-pulse">Embed Author</label>
-          <input bind:value={config.embed.author}
-                 placeholder="author"
-                 class="input input-accent rounded-sm focus:translate-y-2" />
+          <input bind:value={config.embed.author} placeholder="author" class="input input-accent rounded-sm focus:translate-y-2" />
         </div>
 
         <div class="form-control w-full my-2">
           <label class="animate-pulse">Embed Author URL</label>
-          <input bind:value={config.embed["author-url"]}
-                 placeholder="author-url"
-                 class="input input-accent rounded-sm focus:translate-y-2" />
+          <input
+            bind:value={config.embed["author-url"]}
+            placeholder="author-url"
+            class="input input-accent rounded-sm focus:translate-y-2"
+          />
         </div>
       </div>
     </details>
 
     <div class="w-full col-span-4">
-      <label class="cursor-pointer max-w-2xl ">
-        <input bind:checked={agreed_to_tos} type="checkbox"
-               class="checkbox border-gray-400 ease-in-out duration-400 transition-colors checkbox-error" />
+      <label class="cursor-pointer max-w-2xl">
+        <input
+          bind:checked={agreed_to_tos}
+          type="checkbox"
+          class="checkbox border-gray-400 ease-in-out duration-400 transition-colors checkbox-error"
+        />
         <span class="label-text">
           I accept the ascella
           <a href="/rules" target="_blank" class="underline">
@@ -274,56 +269,48 @@
       <span class="animate-ping"><DL /></span>
       <span> Download Config (.sxcu) </span>
     </button>
-    <button name="xy-type" value="desktop" type="submit"
-            class="btn btn-primary col-span-1"
-            on:click={() => (uploadToDesktop = true)}>
+    <button name="xy-type" value="desktop" type="submit" class="btn btn-primary col-span-1" on:click={() => (uploadToDesktop = true)}>
       <span> Upload config to ascella desktop </span>
     </button>
-
   </form>
   <div class="py-3 text-center">
     <p class="col-span-2">
       On linux or mac without sharex? no problem try out
-      <a href="https://github.com/ascellahost/gui"
-         class="link link-hover link-secondary"
-         on:click={save}
-         target="_blank">
+      <a href="https://github.com/ascellahost/gui" class="link link-hover link-secondary" on:click={save} target="_blank">
         The desktop app <OpenInNew class="inline-block align-middle " />
       </a>
     </p>
-    <a href="/upload" on:click={save} class="link link-hover link-primary font-extrabold text-xl">Or try the web version <OpenInNew class="inline-block align-middle " /></a>
+    <a href="/upload" on:click={save} class="link link-hover link-primary font-extrabold text-xl"
+      >Or try the web version <OpenInNew class="inline-block align-middle " /></a
+    >
   </div>
-
 </div>
 
-
 <style>
+  /* KINDLY STOLEN FROM https://css-tricks.com/how-to-animate-the-details-element/*/
 
-    /* KINDLY STOLEN FROM https://css-tricks.com/how-to-animate-the-details-element/*/
+  summary {
+    padding: 1rem;
+    display: block;
+    padding-left: 2.2rem;
+    position: relative;
+    cursor: pointer;
+  }
 
-    summary {
-        padding: 1rem;
-        display: block;
-        padding-left: 2.2rem;
-        position: relative;
-        cursor: pointer;
-    }
+  summary:before {
+    content: "";
+    border-width: 0.4rem;
+    border-style: solid;
+    border-color: transparent transparent transparent #fff;
+    position: absolute;
+    top: 1.3rem;
+    left: 1rem;
+    transform: rotate(0);
+    transform-origin: 0.2rem 50%;
+    transition: 0.25s transform ease;
+  }
 
-    summary:before {
-        content: '';
-        border-width: .4rem;
-        border-style: solid;
-        border-color: transparent transparent transparent #fff;
-        position: absolute;
-        top: 1.3rem;
-        left: 1rem;
-        transform: rotate(0);
-        transform-origin: .2rem 50%;
-        transition: .25s transform ease;
-    }
-
-    details[open] > summary:before {
-        transform: rotate(90deg);
-    }
-
+  details[open] > summary:before {
+    transform: rotate(90deg);
+  }
 </style>
