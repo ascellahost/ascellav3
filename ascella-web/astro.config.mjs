b49b83be5bd5eaabb591ed7f75aedb1062d22ad6
kingstefan26 <70776766+kingstefan26@users.noreply.github.com>
2023-07-09 02:36:16 +0200

    remove @frontendista/astro-html-minify in favor of astro's `compressHTML: true`

--- ascella-web/astro.config.mjs
+++ ascella-web/astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
-import htmlMinify from "@frontendista/astro-html-minify";
-import AstroPWA from '@vite-pwa/astro'
 
 import mdx from "@astrojs/mdx";
 
@@ -12,11 +10,11 @@ export default defineConfig({
   integrations: [
     svelte(),
     tailwind(),
-    mdx(),
-    htmlMinify()
+    mdx()
   ],
   output: "server",
   adapter: cloudflare({
     mode: "directory"
-  })
+  }),
+  compressHTML: true
 });
